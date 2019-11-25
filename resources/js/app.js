d3a634afd7fd5c91a564291fc7a988ce1ae9a7b3
Robert Stefanic <rstefanic72@gmail.com>
2019-11-24 22:49:56 -0600

    Added new article (written-article) Vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,7 @@ window.Vue = require('vue');
 
 Vue.component('new-comment', require('./components/NewCommentComponent.vue').default);
 Vue.component('comment', require('./components/CommentComponent.vue').default);
+Vue.component('written-article', require('./components/ArticleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
