8ffbbc0201d15d9f5bac292b9037147124b41f35
Robert Stefanic <rstefanic72@gmail.com>
2019-11-25 22:09:06 -0600

    Renamed 'article-written' component to 'article-card'

---
+++ resources/js/components/ArticleCardComponent.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="article-card col-md-4">
+        <div class="article-card-contents">
+            <a v-bind:href="slug">
+                <div class='article-img' v-bind:style="articleImageBackground"></div>
+                <h3>{{ article_title }}</h3>
+                <p>{{ summary }}</p>
+                <p>{{ author }}</p>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['article_title', 'article_img', 'author', 'summary', 'slug'],
+
+        computed: {
+            articleImageBackground: function() {
+                return "background-image: url(" + this.article_img + ")";
+            },
+
+            altImgText: function() {
+                return "Image for " + this.article_title;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .article-card {
+        margin: 0;
+        height: 500px;
+        max-height: 500px;
+        color: black;
+    }
+
+    .article-card-contents {
+        padding: 2% 3%;
+    }
+
+    .article-card a {
+        color: black;
+    }
+
+    .article-card a:hover {
+        text-decoration: none;
+    }
+
+    .article-img {
+        max-height: 250px;
+        height: 250px;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        margin-bottom: 6%;
+        overflow: hidden;
+        transition: transform .2s;
+        object-fit: cover;
+    }
+
+    .article-img:hover {
+        transform: scale(1.1);
+    }
+</style>
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,84 +1,90 @@
-{{--<!DOCTYPE html>--}}
-{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
-{{--    <head>--}}
-{{--        <meta charset="utf-8">--}}
-{{--        <meta name="viewport" content="width=device-width, initial-scale=1">--}}
-
-{{--        <title>Laravel</title>--}}
-
-{{--        <!-- Fonts -->--}}
-{{--        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">--}}
-
-{{--        <!-- Styles -->--}}
-{{--        <style>--}}
-{{--            html, body {--}}
-{{--                background-color: #fff;--}}
-{{--                color: #636b6f;--}}
-{{--                font-family: 'Nunito', sans-serif;--}}
-{{--                font-weight: 200;--}}
-{{--                height: 100vh;--}}
-{{--                margin: 0;--}}
-{{--            }--}}
-
-{{--            .full-height {--}}
-{{--                height: 100vh;--}}
-{{--            }--}}
-{{--            .position-ref {--}}
-{{--                position: relative;--}}
-{{--            }--}}
-
-{{--            .top-right {--}}
-{{--                position: absolute;--}}
-{{--                right: 10px;--}}
-{{--                top: 18px;--}}
-{{--            }--}}
-
-{{--            .content {--}}
-{{--                text-align: center;--}}
-{{--            }--}}
-
-{{--            .links > a {--}}
-{{--                color: #636b6f;--}}
-{{--                padding: 0 25px;--}}
-{{--                font-size: 13px;--}}
-{{--                font-weight: 600;--}}
-{{--                letter-spacing: .1rem;--}}
-{{--                text-decoration: none;--}}
-{{--                text-transform: uppercase;--}}
-{{--            }--}}
-{{--        </style>--}}
-{{--    </head>--}}
-{{--    <body>--}}
 @extends('layouts.app')
 
-@section('content')
-    <div class="flex-center position-ref full-height">
-{{--        <div class="top-right links">--}}
-{{--            <a href="{{ url('/') }}">Home</a>--}}
-{{--            <a href="{{ url('/donate') }}">Donate</a>--}}
-{{--            <a href="{{ url('/about') }}">About</a>--}}
-{{--            @auth--}}
-{{--                <a href="/profile/{{ Auth::user()->username }}">Profile</a>--}}
-{{--            @else--}}
-{{--                <a href="{{ route('register') }}">Register</a>--}}
-{{--                <a href="{{ route('login') }}">Login</a>--}}
-{{--            @endauth--}}
-{{--        </div>--}}
+@section('title', 'Home')
 
+@section('content')
+<!-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+<!--         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+ -->
+        <!-- Styles -->
+<!--         <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+        </style>
+    </head>
+    <body>
+ --><!--     <div class="flex-center position-ref full-height">
+         <div class="top-right links">
+            <a href="{{ url('/') }}">Home</a>
+            <a href="{{ url('/donate') }}">Donate</a>
+            <a href="{{ url('/about') }}">About</a>
+            @auth
+                <a href="/profile/{{ Auth::user()->username }}">Profile</a>
+            @else
+                <a href="{{ route('register') }}">Register</a>
+                <a href="{{ route('login') }}">Login</a>
+            @endauth
+        </div>
+ -->
         <div class="content">
-            <div class="title">
-                Media In Focus
+            <div class="home-hero-image" style="background-image: url({{ $featured_article->articleHeroImage() }});">
+                <div class="home-hero-text">
+                    <h1>{{ $featured_article-> title }}</h1>
+                    <p class="home-hero-summary">{{ $featured_article->summary }}</p>
+                    <p class="home-hero-author">{{ $featured_article->user->name }}</p>
+                    <a class="btn btn-info" href="{{ $featured_article->buildFullPath() }}">Read More</a>
+                </div>
             </div>
 
             <div class="row article-container d-flex align-items-center">
                 @foreach($articles as $article)
-                    <written-article
+                    <article-card
                         article_title="{{ $article->title }}"
                         article_img="{{ $article->articleHeroImage() }}"
                         author="{{ $article->user->name }}"
                         summary="{{ $article->summary }}"
-                        slug="/article/{{ $article->slug }}"
-                    ></written-article>
+                        slug="{{ $article->buildFullPath() }}"
+                    ></article-card>
                 @endforeach
             </div>
         </div>
