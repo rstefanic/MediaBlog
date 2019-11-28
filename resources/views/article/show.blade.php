05ddb9d874191ddf2971ac8a3ab496be6075a0d4
Robert Stefanic <rstefanic72@gmail.com>
2019-11-28 00:47:16 -0600

    Refactored the model that was passed into the show.blade.php view

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -2,32 +2,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="hero" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('{{ $image }}');">
+<div class="hero" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('{{ $article->articleHeroImage() }}');">
     <div class="hero-text">
         <h1>
-            {{ $title }}
+            {{ $article->title }}
         </h1>
-        <h3>{{ $summary }}</h3>
-        <h5>By: {{ $author }} </h5>
+        <h3>{{ $article->summary }}</h3>
+        <h5>By: {{ $article->user->name }} </h5>
     </div>
 </div>
 
 <div class="container py-5">
     {!! $body !!}
 
-    <div class="card author-info">
-        <div class="card-header"><h2>Author</h2></div>
+    <div class="card author-info mt-5">
+        <div class="card-header d-flex justify-content-between">
+            <h2>Author</h2>
+            @can('update', $article)
+                <a href="/article/{{ $article->slug }}/edit" class="btn btn-primary">Edit This Post</a>
+            @endcan
+        </div>
         <div class="card-body">
             <div class="d-flex align-items-center">
                     <div class="pr-3">
-                        <a href="/profile/{{ $author_username }}">
-                            <img class="rounded-circle w-100" src="{{ $author_image }}" alt="Image of the Author">
+                        <a href="/profile/{{ $article->user->username }}">
+                            <img class="rounded-circle w-100" src="{{ $article->user->profileImage() }}" alt="Image of the Author">
                         </a>
                     </div>
                 <div>
-                    <h3><a href="/profile/{{ $author_username }}">{{ $author }}</a></h3>
+                    <h3><a href="/profile/{{ $article->user->username }}">{{ $article->user->username }}</a></h3>
                     <div class="bio pt-3">
-                        {{ $bio }}
+                        {{ $article->user->bio }}
                     </div>
                 </div>
             </div>
@@ -35,13 +40,13 @@
     </div>
 
     <div class="card comments-section mt-5">
-            <div class="card-header"><h2>Comments</h2></div>
+        <div class="card-header"><h2>Comments</h2></div>
             <div class="card-body">
                 @auth
                     <new-comment
-                        article-id="{{ $article_id }}"
-                        profile_picture="{{ $user->profileImage() }}"
-                        username="{{ $user->username }}"
+                        article-id="{{ $article->id }}"
+                        profile_picture="{{ $current_user->profileImage() }}"
+                        username="{{ $current_user->username }}"
                     ></new-comment>
                 @endauth
 
@@ -53,7 +58,7 @@
                         date="{{ $comment->created_at }}"
                         comment="{{ $comment->comment }}"
                     ></comment>
-                    @endforeach
+                @endforeach
             </div>
     </div>
 </div>
