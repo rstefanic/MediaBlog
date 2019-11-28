47eaa0087153580a3f42a456b4d646501d44e519
Robert Stefanic <rstefanic72@gmail.com>
2019-11-28 14:30:55 -0600

    Cleaned up zombie code; Made featured article load conditional

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,70 +3,8 @@
 @section('title', 'Home')
 
 @section('content')
-<!-- <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-<!--         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
- -->
-        <!-- Styles -->
-<!--         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-        </style>
-    </head>
-    <body>
- --><!--     <div class="flex-center position-ref full-height">
-         <div class="top-right links">
-            <a href="{{ url('/') }}">Home</a>
-            <a href="{{ url('/donate') }}">Donate</a>
-            <a href="{{ url('/about') }}">About</a>
-            @auth
-                <a href="/profile/{{ Auth::user()->username }}">Profile</a>
-            @else
-                <a href="{{ route('register') }}">Register</a>
-                <a href="{{ route('login') }}">Login</a>
-            @endauth
-        </div>
- -->
         <div class="content">
+            @if($featured_article->count() == 1)
             <div class="home-hero-image" style="background-image: url({{ $featured_article->articleHeroImage() }});">
                 <div class="home-hero-text">
                     <h1>{{ $featured_article-> title }}</h1>
@@ -75,6 +13,7 @@
                     <a class="btn btn-info" href="{{ $featured_article->buildFullPath() }}">Read More</a>
                 </div>
             </div>
+            @endif
 
             <div class="row article-container d-flex align-items-center">
                 @foreach($articles as $article)
