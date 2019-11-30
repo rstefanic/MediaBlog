d627d90b2522b8387588edb3a05d1ea91cd9a395
Robert Stefanic <rstefanic72@gmail.com>
2019-11-30 11:58:43 -0600

    Added Dummy Copy to About page for the time being

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,6 +6,15 @@
     <div class="container py-4">
         <div class="row justify-content-center">
             <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut augue vestibulum risus gravida efficitur ut a felis. Nam mollis ut neque non commodo. Nullam ac magna ac sem ultrices tempor at a augue. Duis maximus arcu quis mi vestibulum hendrerit. Sed ac ante sed diam porta suscipit. Nulla sit amet elementum justo. Suspendisse arcu purus, tincidunt eget tortor ut, rutrum suscipit ex. Vestibulum blandit enim velit. Proin nec tristique eros. Duis sollicitudin ultrices nisi, et blandit dui. Praesent nulla nulla, maximus quis condimentum eu, pellentesque id libero. Maecenas posuere odio sit amet leo mattis, ut elementum metus ornare. Vestibulum elementum tristique magna a placerat. Aliquam vitae viverra libero. Vivamus nec aliquet justo.</p>
+
+            <p>Vivamus et congue ligula. Maecenas viverra ligula eget nunc facilisis mollis non eu velit. Nunc viverra, ante aliquet sagittis vulputate, sapien enim aliquam sapien, ut fringilla libero mi ac sem. Duis ex nisi, viverra ut mollis ut, sollicitudin in est. Cras justo lectus, tempor sit amet pharetra eu, volutpat iaculis nibh. Quisque maximus eleifend felis eget varius. Nulla eu dui sit amet nibh sodales pharetra. Donec ultrices efficitur urna ut tempus. Sed sem quam, consequat ac dictum vitae, accumsan ut leo.</p>
+
+            <p>Duis vitae urna facilisis, efficitur tortor ac, accumsan nunc. Sed justo risus, lacinia sed nisl eget, aliquet molestie mauris. Pellentesque cursus, lacus in tincidunt iaculis, diam orci molestie magna, vel semper dui tellus non sapien. Phasellus et nisl vulputate, consectetur augue eu, efficitur mauris. Curabitur feugiat orci sed elementum iaculis. Quisque semper quis neque id porta. Pellentesque rutrum, quam ut laoreet accumsan, enim nulla aliquet ante, sit amet viverra neque arcu ut lacus. Maecenas vitae fringilla odio.</p>
+
+            <p>Curabitur consectetur aliquet orci, id aliquet diam luctus vitae. Nunc non lorem ac est fermentum euismod. Ut tristique, est vel varius iaculis, quam dui maximus ligula, vitae efficitur nibh purus sit amet dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut aliquam elit vitae dignissim venenatis. Donec eu quam nunc. Donec in turpis ac neque elementum congue et ac orci. Aenean dictum odio quis posuere aliquam.</p>
+
+            <p>Donec eget nisl eu eros eleifend venenatis eu ac dui. Suspendisse et nibh ut est congue blandit. Aenean efficitur ex id ullamcorper gravida. Suspendisse potenti. Sed at metus varius, feugiat felis eu, iaculis ipsum. Fusce pharetra sit amet sem iaculis ullamcorper. Maecenas blandit sed odio vel faucibus. Aenean at efficitur purus, sed sagittis lectus. Etiam iaculis ante in suscipit lobortis. </p>
         </div>
     </div>
-    @endsection
+@endsection
