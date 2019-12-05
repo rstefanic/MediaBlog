674cc1a1f4e0bca218f5c114fde371f7ed2a649e
Robert Stefanic <rstefanic72@gmail.com>
2019-12-04 20:52:27 -0600

    Finsished creating the form on TagController@index to post to TagController@store

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Tags')
 
 @section('content')
-
+<div class="container py-4">
     <div class="row">
         <div class="col-md-8">
             <h1>Tags</h1>
@@ -13,5 +13,16 @@
                 @endforeach
             </ul>
         </div>
-        
+
+        <div class="col-md-4">
+            {!! Form::open(['route' => 'tags.store', 'method' => 'POST']) !!}
+                <h2>New Tag</h2>
+                {{ Form::label('name', 'Name:') }}
+                {{ Form::text('name', null, ['class' => 'form-control']) }}
+
+                {{ Form::submit('Create New Tag', ['class' => 'btn btn-primary btn-block btn-h1-spacing mt-3']) }}
+            {!! Form::close() !!}
+        </div>
+    </div>
+</div>
 @endsection
